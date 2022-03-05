3b60bba0f2a3ee94d3d43bc07cb9dcb4bd7e2512
Vivian Chen <vivianchen@dhcp-vl2041-30505.eduroam.cornell.edu>
2022-03-05 12:54:24 -0500

    made modifications

--- a.py
+++ a.py
@@ -1,6 +1,9 @@
 some_list  = [1,2,3]
 def func(some_list):
-    some_list.append(4)
+    some_list.append(4,7,8,9)
     print(some_list)
-
+    def func2():
+        print(some_list)
+    func2()
+    print(some_list)
 func(some_list)
